42c66bc77f13b10680f25cbda3a37afa2400e367
junmojin <o3o060@naver.com>
2026-06-10 22:59:06 +0900

    Spring Boot 프로젝트 초기 생성

---
+++ src/main/java/com/junmo/certificatesystem/CertificateSystemApplication.java
@@ -0,0 +1,13 @@
+package com.junmo.certificatesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junmo/certificatesystem/CertificateSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junmo.certificatesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
