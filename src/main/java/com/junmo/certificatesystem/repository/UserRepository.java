7a041920e446a18431569e0f3d3696640d52af19
junmojin <o3o060@naver.com>
2026-06-16 22:50:39 +0900

    feat(user): userId 조회 메서드 추가

--- src/main/java/com/junmo/certificatesystem/repository/UserRepository.java
+++ src/main/java/com/junmo/certificatesystem/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.junmo.certificatesystem.repository;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.junmo.certificatesystem.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserId(String userId);
+
+    boolean existsByUserId(String userId);
 }
