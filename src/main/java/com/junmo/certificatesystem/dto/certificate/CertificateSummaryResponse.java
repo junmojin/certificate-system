a666a3425dc6e0e312a1ba6afc5c2383170c61bb
junmojin <o3o060@naver.com>
2026-06-25 00:35:39 +0900

    feat(certificate): 신청 내역 목록 응답 DTO 추가

---
+++ src/main/java/com/junmo/certificatesystem/dto/certificate/CertificateSummaryResponse.java
@@ -0,0 +1,16 @@
+package com.junmo.certificatesystem.dto.certificate;
+
+import com.junmo.certificatesystem.common.enums.CertificateStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CertificateSummaryResponse {
+
+    private final Long id;
+    private final String companyName;
+    private final String position;
+    private final CertificateStatus status;
+}
